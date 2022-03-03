bfe7896916dfcdcee99eda19188305e6c6082835
Matthew Carpenter <matthew.carpenter@broad-path.com>
2022-03-03 16:19:31 -0700

    added a prefab.

---
+++ Assets/Scenes/log_this.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class log_this : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("A Cube was built.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
